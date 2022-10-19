23ea8b945fa53839fc0d642b3ab60d51664a7ec7
Alice Medvedeva <alisa.medvedeva@optimax.dev>
2022-10-19 14:12:02 +0500

    last changes

--- call__apply.js
+++ call__apply.js
@@ -1,20 +1,22 @@
 // 'use strict';
 
-// const obj = {
-//     name: 1,
-//     born: 20,
-// };
+const obj = {
+    name: 1,
+    born: 20,
+};
 //
-// const mas = [3, 5];
+const mas = [3, 5];
 //
-// function f1(a, b) {
-//     let c = this.born;
-//     const sum = a + b + c;
-//     console.log(sum);
-// }
+function f1(a, b) {
+    let c = this.born;
+    const sum = a + b + c;
+    console.log(sum);
+}
 //
 // f1.call(obj, 3, 5);
-// f1.apply(obj, mas);
+f1.apply(obj, mas);
+const r = new Boolean(0);
+console.log('0 ', r)
 //
 // // задача на call apply bind
 // function func () {
